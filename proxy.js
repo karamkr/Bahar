df624c9fcc6ca90afa0325bfd2991398a1b284de
karamkr <157712604+karamkr@users.noreply.github.com>
2025-07-13 08:32:42 +0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,32 +1,26 @@
 const fetch = require("node-fetch");
 
-exports.handler = async function(event, context) {
+exports.handler = async function(event) {
   const targetUrl = event.queryStringParameters.url;
   if (!targetUrl) {
-    return {
-      statusCode: 400,
-      body: "Missing URL",
-    };
+    return { statusCode: 400, body: "Missing URL" };
   }
 
   try {
     const response = await fetch(targetUrl);
     const contentType = response.headers.get("content-type");
+    const buffer = await response.buffer();
 
-    const body = await response.buffer();
     return {
       statusCode: 200,
       headers: {
         "Content-Type": contentType,
         "Access-Control-Allow-Origin": "*"
       },
-      body: body.toString("base64"),
+      body: buffer.toString("base64"),
       isBase64Encoded: true
     };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: "Fetch failed: " + err.message,
-    };
+  } catch (error) {
+    return { statusCode: 500, body: "Proxy error: " + error.message };
   }
 };
